68d876775aec03347d4a9b6b4448a8da603e3c0c
kburger <burger.github@gmail.com>
2016-07-07 10:22:15 +0200

    more angular solution to the select options

--- demo/home/home-controller.js
+++ demo/home/home-controller.js
@@ -1,7 +1,12 @@
 app.controller('HomeCtrl', function($scope) {
-  $scope.questionChanged = function(id) {
-    console.log('selected question', id);
-    $scope.selectedQuestion = id;
+  $scope.selectedQuestion = null;
+  $scope.questions = [
+    {value: 1, name: 'The first question'},
+    {value: 2, name: 'Question #2'}
+  ];
+  
+  $scope.questionChanged = function() {
+    console.log('selected question', $scope.selectedQuestion);
   };
   
   $scope.process = function() {
