3dabf6de4ac474aecb9b681af187407a1638da2e
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-08-02 10:39:15 +0200

    Cache with dynamic user input

--- demo/cache/cache-controller.js
+++ demo/cache/cache-controller.js
@@ -1,15 +1,22 @@
 app.controller('CacheCtrl', function($scope, $rootScope, FDP, Caching) {    
   $scope.loadingData = false;
   
-  var fairDataPoints=[
-                         {name:"rdconnect", url:"http://semlab1.liacs.nl:8080/fdp"},
-                         {name:"dtl", url:"http://145.100.59.120:8082/fdp"}
-                         ];
+  $scope.fairDataPoints=[{name: "RD connect FDP", url:"http://semlab1.liacs.nl:8080/fdp"}];
   
+  $scope.addFdp = function () {
+    $scope.fairDataPoints.push({ 
+      url: "", name:""
+    });
+  };
+  
+  $scope.removeFdp = function(fdp) { 
+    var index = $scope.fairDataPoints.indexOf(fdp);
+    $scope.fairDataPoints.splice(index, 1);     
+  };
   
   $scope.cache = function() {
     $scope.loadingData = true;
-    FDP.load(fairDataPoints); 
+    FDP.load($scope.fairDataPoints); 
     $rootScope.$on('fdp-data-loaded', function() {
       console.log('Caching is done');
       $scope.loadingData = false;
